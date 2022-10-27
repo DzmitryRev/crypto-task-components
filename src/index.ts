e183adca8cdcc3c3cac8c934d45b78dfc86fd961
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-10-27 19:26:15 +0300

    feat: add exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import Button from "./Button/Button";
 import ButtonLink from "./Link/Link";
+import Loading from "./Loading/Loading";
+import Modal from "./Modal/Modal";
+import Pagination from "./Pagination/Pagination";
+import Table from "./Table/Table";
+import TableRow from "./TableRow/TableRow";
 
-export { Button, ButtonLink };
+export { Button, ButtonLink, Loading, Modal, Pagination, Table, TableRow };
