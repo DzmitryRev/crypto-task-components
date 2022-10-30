5d82a8f369ebdcbc08e246009e835668656ddfd0
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-10-30 23:08:29 +0300

    feat: add as props to button comp

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,13 +1,13 @@
 import React, { PropsWithChildren } from "react";
 import StyledButton from "./StyledButton";
 
-interface ButtonPropsType extends React.ComponentProps<"button"> {
+interface ButtonPropsType extends React.ComponentProps<typeof StyledButton> {
   color: "blue" | "green" | "red";
 }
 
-function Button({ color, children }: PropsWithChildren<ButtonPropsType>) {
+function Button({ color, children, as }: PropsWithChildren<ButtonPropsType>) {
   return (
-    <StyledButton data-testid="button" color={color}>
+    <StyledButton as={as} data-testid="button" color={color}>
       {children}
     </StyledButton>
   );
